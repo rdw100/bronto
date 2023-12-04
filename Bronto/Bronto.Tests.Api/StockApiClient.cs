900ba068cca9a7388676a9a38650326a040c9c79
rdw100 <ryandustinwright@hotmail.com>
2023-12-04 16:21:33 -0500

    Initial test.  Initial api.

---
+++ Bronto/Bronto.Tests.Api/StockApiClient.cs
@@ -0,0 +1,70 @@
+﻿using Bronto.Tests.Api.Models;
+using Newtonsoft.Json;
+
+namespace Bronto.Tests.Api
+{
+    public class StockApiClient
+    {
+        private readonly string _apiKey;
+        private readonly HttpClient _client;
+
+        public StockApiClient(string key, HttpClient client)
+        {
+            _apiKey = key;
+            _client = client;
+        }
+
+        public async Task<StockDataTimeSeries> GetTimeSeriesAsync(string symbol, string interval = "1min")
+        {
+            try
+            {
+                string endpoint = "https://api.twelvedata.com/time_series?symbol=" + symbol + "&interval=" + interval +
+                                  "&apikey=" + _apiKey;
+
+                var response = await _client.GetAsync(endpoint);
+                string responseString = await response.Content.ReadAsStringAsync();
+                var jsonResponse = JsonConvert.DeserializeObject<StockTimeSeries>(responseString);
+                List<TimeSeriesValues> values = new List<TimeSeriesValues>();
+                var tsStockValues = jsonResponse?.Values;
+                if (tsStockValues != null)
+                {
+                    values.AddRange(tsStockValues.Select(v => new TimeSeriesValues()
+                    {
+                        Datetime = v?.Datetime ?? DateTime.MinValue,
+                        Open = Convert.ToDouble(v?.Open),
+                        High = Convert.ToDouble(v?.High),
+                        Low = Convert.ToDouble(v?.Low),
+                        Close = Convert.ToDouble(v?.Close),
+                        Volume = v.Volume
+                    }));
+                }
+
+                StockDataTimeSeries timeSeries = new StockDataTimeSeries()
+                {
+                    Symbol = jsonResponse?.Meta?.Symbol,
+                    Interval = jsonResponse?.Meta?.Interval,
+                    ExchangeTimezone = jsonResponse?.Meta?.ExchangeTimezone,
+                    Exchange = jsonResponse?.Meta?.Exchange,
+                    Type = jsonResponse?.Meta?.Type,
+                    Currency = jsonResponse?.Meta?.Currency,
+                    Values = values
+                };
+
+                if (!string.IsNullOrEmpty(timeSeries?.Symbol) && values.Count != 0)
+                    return timeSeries;
+                timeSeries.ResponseStatus = Enums.StockDataClientResponseStatus.StockDataApiError;
+                timeSeries.ResponseMessage = "Invalid symbol or API key";
+
+                return timeSeries;
+            }
+            catch (Exception e)
+            {
+                return new StockDataTimeSeries()
+                {
+                    ResponseStatus = Enums.StockDataClientResponseStatus.StockDataSharpError,
+                    ResponseMessage = e.ToString()
+                };
+            }
+        }
+    }
+}
---
+++ Bronto/Bronto.Tests.Api/StockApi_Quote_Tests.cs
@@ -0,0 +1,52 @@
+using FluentAssertions;
+using RichardSzalay.MockHttp;
+using Microsoft.Extensions.Configuration;
+using Bronto.Tests.Api.Models;
+
+namespace Bronto.Tests.Api
+{
+    public class StockApi_Base
+    {
+    }
+
+    public class StockApi_GetQuote_Tests : StockApi_Base
+    {
+        [Fact]
+        public async void StockApi_ShouldGetTimeSeriesAsync_ReturnsTrue()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration config = builder.Build();
+
+            string keyString = config.GetSection("AppSettings")["Key"];
+            string hostString = config.GetSection("AppSettings")["Host"];
+
+            // ARRANGE
+            var mockHttp = new MockHttpMessageHandler();
+            
+            mockHttp
+                .When($"https://{hostString}/*")
+                .Respond("application/json", "{\"meta\":{\"symbol\":\"AAPL\",\"interval\":\"1min\",\"currency\":\"USD\",\"exchange_timezone\":\"America/New_York\",\"exchange\":\"NASDAQ\",\"type\":\"Common Stock\"},\"values\":[{\"datetime\":\"2023-12-01 00:00:00\",\"open\":\"191.13000\",\"high\":\"191.24500\",\"low\":\"191.12700\",\"close\":\"191.24500\",\"volume\":\"44707\"}],\"status\":\"ok\"}");
+
+            StockApiClient stockApiClient = new StockApiClient(keyString, mockHttp.ToHttpClient());
+
+            // ACT
+            var response = await stockApiClient.GetTimeSeriesAsync("AAPL");
+
+            // ASSERT
+            response?.ResponseStatus.Should().Be(Enums.StockDataClientResponseStatus.Ok);
+            response?.ResponseMessage.Should().Be("RESPONSE_OK");
+            response?.Values[0]?.Datetime.Should().Be(new DateTime(2023, 12, 1, 00, 00, 00));
+            response?.ExchangeTimezone.Should().Be("America/New_York");
+            response?.Exchange.Should().Be("NASDAQ");
+            response?.Type.Should().Be("Common Stock");
+            response?.Values[0]?.Open.Should().Be(191.13000);
+            response?.Values[0]?.High.Should().Be(191.24500);
+            response?.Values[0]?.Low.Should().Be(191.12700);
+            response?.Values[0]?.Close.Should().Be(191.24500);
+            response?.Values[0]?.Volume.Should().Be(44707);
+        }
+    }
+}
