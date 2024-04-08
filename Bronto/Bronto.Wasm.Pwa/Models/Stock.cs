a925e19ff8a2966fffc45c83ac403677c1cd5f90
rdw100 <ryandustinwright@hotmail.com>
2024-04-08 15:55:44 -0400

    Initial PWA.  Created stocks page.  Created a Blazor WebAssembly Progressive Web App (PWA) that displays a ticker symbol and price.

---
+++ Bronto/Bronto.Wasm.Pwa/Models/Stock.cs
@@ -0,0 +1,8 @@
+﻿namespace Bronto.Wasm.Pwa.Models
+{
+    public class Stock
+    {
+        public string TickerSymbol { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Bronto/Bronto.Wasm.Pwa/Service/StockService.cs
@@ -0,0 +1,20 @@
+﻿namespace Bronto.Wasm.Pwa.Service
+{
+    public class StockService
+    {
+        private readonly HttpClient _httpClient;
+
+        public StockService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<decimal> GetStockPriceAsync(string tickerSymbol)
+        {
+            // Add logic here to fetch stock price from an API
+            // For this example, we'll just return a random price
+            Random rnd = new Random();
+            return await Task.FromResult<decimal>(rnd.Next(5000, 20000) / 100);
+        }
+    }
+}
