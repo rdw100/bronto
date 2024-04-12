ad619db3fea51fc8167c90457b30c00be48dbaf2
rdw100 <ryandustinwright@hotmail.com>
2024-04-12 08:21:54 -0400

    Added TimeSeries JSON pasted as class.

---
+++ Bronto/Bronto.Models/TimeSeries.cs
@@ -0,0 +1,30 @@
+﻿namespace Bronto.Models
+{
+    public class TimeSeries
+    {
+        public Meta meta { get; set; }
+        public Value[] values { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Meta
+    {
+        public string symbol { get; set; }
+        public string interval { get; set; }
+        public string currency { get; set; }
+        public string exchange_timezone { get; set; }
+        public string exchange { get; set; }
+        public string mic_code { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Value
+    {
+        public string datetime { get; set; }
+        public string open { get; set; }
+        public string high { get; set; }
+        public string low { get; set; }
+        public string close { get; set; }
+        public string volume { get; set; }
+    }
+}
