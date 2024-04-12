a2fa2b5402861ea5d333e25214a7e55c6ffc04bb
rdw100 <ryandustinwright@hotmail.com>
2024-04-12 08:12:32 -0400

    Added time series call with +1 credit API call.

--- Bronto/Bronto.Stocks.Console/Program.cs
+++ Bronto/Bronto.Stocks.Console/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Bronto.Stocks.Console;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 
 var builder = new ConfigurationBuilder();
 builder.SetBasePath(Directory.GetCurrentDirectory())
@@ -8,11 +10,25 @@ IConfiguration config = builder.Build();
 
 string keyString = config.GetSection("AppSettings")["Key"];
 string hostString = config.GetSection("AppSettings")["Host"];
-string uriString = $"https://{hostString}/stocks?symbol=AAPL&exchange=NASDAQ&format=json";
+string subString = config.GetSection("AppSettings")["Subscription"];
+
+// Basic Free 800 api credit limit with 8 WebSockets cedits
+// 1 credit * 3 symbols equals 3 credits
+// Income_statement is @ 3 symbols equals 300 credits
+string uriString = string.Empty;
+
+if (!string.IsNullOrEmpty(subString) && subString == "Basic")
+{
+    uriString = $"https://{hostString}/time_series?symbol=AAPL&interval=1min&format=json&apikey={keyString}";
+}
+else
+{
+    uriString = $"https://{hostString}/stocks?symbol=AAPL&exchange=NASDAQ&format=json";
+}
 
 var client = new HttpClient();
 
-if (string.IsNullOrEmpty(keyString) || string.IsNullOrEmpty(hostString))
+if (!string.IsNullOrEmpty(keyString) && !string.IsNullOrEmpty(hostString))
 {
     var request = new HttpRequestMessage
     {
@@ -30,6 +46,13 @@ if (string.IsNullOrEmpty(keyString) || string.IsNullOrEmpty(hostString))
         response.EnsureSuccessStatusCode();
         var body = await response.Content.ReadAsStringAsync();
         Console.WriteLine(body);
+        
+        // Time series
+        var timeSeries = JsonSerializer.Deserialize<TimeSeries>(body);
+        if (timeSeries.status == "ok")
+        {
+            Console.WriteLine("Received symbol: " + timeSeries.meta["symbol"] + ", close: " + timeSeries.values[0]["close"]);
+        }
     }
 }
 else
---
+++ Bronto/Bronto.Stocks.Console/TimeSeries.cs
@@ -0,0 +1,9 @@
+﻿namespace Bronto.Stocks.Console
+{
+    public class TimeSeries
+    {
+        public Dictionary<string, string> meta { get; set; }
+        public IList<Dictionary<string, string>> values { get; set; }
+        public string status { get; set; }
+    }
+}
