4dacf0232e5b284108e540a02b7803b995b9ba4d
rdw100 <ryandustinwright@hotmail.com>
2024-04-14 17:06:39 -0400

    Added controller

---
+++ Bronto/Bronto.WebApi/Controllers/TimeSeriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bronto.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeSeriesController : ControllerBase
+    {
+        private readonly HttpClient _client;
+
+        public TimeSeriesController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://myapi.stocks.org/data/1.2");
+        }
+    }
+}
