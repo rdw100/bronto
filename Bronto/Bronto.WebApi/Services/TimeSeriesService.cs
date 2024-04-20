32456dddbff55e61841f86bd4a00887c0d397bbe
rdw100 <ryandustinwright@hotmail.com>
2024-04-20 10:18:09 -0400

    Added service class to handle API requests and data parsing.  Refactor service from controller.

---
+++ Bronto/Bronto.WebApi/Services/TimeSeriesService.cs
@@ -0,0 +1,86 @@
+﻿using Bronto.Models.Api;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Bronto.WebApi.Services
+{
+    public class TimeSeriesService
+    {
+        private readonly HttpClient _httpClient;
+        private IConfiguration config { get; set; }
+
+        protected internal string Key { get; set; }
+        protected internal string Host { get; set; }
+        protected internal string uriString { get; set; }
+
+        public TimeSeriesService()
+        {
+            Key = config.GetSection("AppSettings")["Key"];
+            Host = config.GetSection("AppSettings")["Host"];
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(Host);
+        }
+
+        public async Task<StockDataTimeSeries> GetTimeSeriesAsync(string symbol, string interval = "1min")
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync($"time_series?symbol={symbol}&interval={interval}&apikey={Key}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    var jsonResponse = JsonConvert.DeserializeObject<StockTimeSeries>(responseString);
+                    List<TimeSeriesValues> values = new List<TimeSeriesValues>();
+                    var tsStockValues = jsonResponse?.Values;
+                    if (tsStockValues != null)
+                    {
+                        values.AddRange(tsStockValues.Select(v => new TimeSeriesValues()
+                        {
+                            Datetime = v?.Datetime ?? DateTime.MinValue,
+                            Open = Convert.ToDouble(v?.Open),
+                            High = Convert.ToDouble(v?.High),
+                            Low = Convert.ToDouble(v?.Low),
+                            Close = Convert.ToDouble(v?.Close),
+                            Volume = v.Volume
+                        }));
+                    }
+
+                    StockDataTimeSeries timeSeries = new StockDataTimeSeries()
+                    {
+                        Symbol = jsonResponse?.Meta?.Symbol,
+                        Interval = jsonResponse?.Meta?.Interval,
+                        ExchangeTimezone = jsonResponse?.Meta?.ExchangeTimezone,
+                        Exchange = jsonResponse?.Meta?.Exchange,
+                        Type = jsonResponse?.Meta?.Type,
+                        Currency = jsonResponse?.Meta?.Currency,
+                        Values = values
+                    };
+
+                    if (string.IsNullOrEmpty(timeSeries?.Symbol) || values.Count == 0)
+                    {
+                        timeSeries.ResponseStatus = Enums.StockDataClientResponseStatus.StockDataApiError;
+                        timeSeries.ResponseMessage = "Invalid symbol or API key";
+
+                        return timeSeries;
+                    }
+
+                    return timeSeries;
+                }
+                else
+                {
+                    // Handle error response
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new StockDataTimeSeries()
+                {
+                    ResponseStatus = Enums.StockDataClientResponseStatus.StockDataError,
+                    ResponseMessage = ex.ToString()
+                };
+            }
+        }
+    }
+}
