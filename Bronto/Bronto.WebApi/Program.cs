533ce74957423dcbfc99b5129fdbfcecdab543a6
rdw100 <ryandustinwright@hotmail.com>
2024-05-21 08:00:03 -0400

    Added initial rate limiting capabilitites for testing.  Returns 429.

--- Bronto/Bronto.WebApi/Program.cs
+++ Bronto/Bronto.WebApi/Program.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,48 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRateLimiter(limiterOptions =>
+{
+    limiterOptions.OnRejected = (httpContext, _) =>
+    {
+        if (httpContext.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            httpContext.HttpContext.Response.Headers.RetryAfter =
+                ((int)retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+        }
+
+        httpContext.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        httpContext.HttpContext.Response.WriteAsync("Too many requests. Please try again later.");
+
+        return new ValueTask();
+    };
+
+    limiterOptions.GlobalLimiter = PartitionedRateLimiter.CreateChained(
+        PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        {
+            var userAgent = httpContext.Request.Headers.UserAgent.ToString();
+            return RateLimitPartition.GetFixedWindowLimiter(
+                userAgent, _ => new FixedWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 2,
+                    Window = TimeSpan.FromSeconds(60)
+                });
+        }),
+        PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        {
+            var clientIP = httpContext.Connection.RemoteIpAddress!.ToString();
+            return RateLimitPartition.GetFixedWindowLimiter(
+                clientIP, _ => new FixedWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 100,
+                    Window = TimeSpan.FromDays(1)
+                });
+        }));
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -29,6 +74,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRateLimiter();
 
 app.UseCors();
 
