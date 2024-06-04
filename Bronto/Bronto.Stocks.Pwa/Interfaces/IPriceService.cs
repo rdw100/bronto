ca16e09258c8ecc225773246c0ffd6aa1bb72f96
rdw100 <ryandustinwright@hotmail.com>
2024-06-04 11:08:50 -0400

    Added list of symbols to price service.

--- Bronto/Bronto.Stocks.Pwa/Interfaces/IPriceService.cs
+++ Bronto/Bronto.Stocks.Pwa/Interfaces/IPriceService.cs
@@ -4,6 +4,7 @@ namespace Bronto.Stocks.Pwa.Interfaces
 {
     public interface IPriceService
     {
+        public List<string> Symbols { get; }
         Task<RealTimePrice> GetPriceAsync(string symbol);
     }
 }
