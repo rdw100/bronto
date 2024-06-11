47d863c658d203d8a53b7fc41a7fb41d41122de3
rdw100 <ryandustinwright@hotmail.com>
2024-06-11 09:18:01 -0400

    Added client-side HTTP handler to rate limit from the client side.  Client-side policy does not exceed server-side limits, server-side limit is met first and remaining requests receive a 429. Adjust server-side limits for expected result.

---
+++ Bronto/Bronto.Tests.RateLimit/Program.cs
@@ -0,0 +1,51 @@
+﻿using Bronto.Tests.RateLimit;
+using System.Threading.RateLimiting;
+
+/*
+Create a client-side HTTP handler that rate limits the number of requests it sends
+Client-side policy does not exceed server-side limits, server-side limit is met
+first and remaining requests receive a 429. Adjust server-side limits for expected result
+*/
+Thread.Sleep(8000);
+
+var options = new FixedWindowRateLimiterOptions
+    {
+        AutoReplenishment = true,
+        PermitLimit = 8,
+        Window = TimeSpan.FromSeconds(60)
+    };
+
+// Create an HTTP client with the client-side rate limited handler.
+using HttpClient client = new(
+    handler: new ClientSideRateLimitedHandler(
+        limiter: new FixedWindowRateLimiter(options)        
+        ));
+
+// Create 8 urls with a unique query string.
+var oneHundredUrls = Enumerable.Range(1, 8).Select(
+    i => $"https://localhost:7085/api/Price?symbol=aapl&iteration={i:0#}");
+
+// Flood the HTTP client with requests.
+var floodOneThroughFourTask = Parallel.ForEachAsync(
+    source: oneHundredUrls.Take(1..4),
+    body: (url, cancellationToken) => GetAsync(client, url, cancellationToken));
+
+var floodFiveThroughEightTask = Parallel.ForEachAsync(
+    source: oneHundredUrls.Take(^5..),
+    body: (url, cancellationToken) => GetAsync(client, url, cancellationToken));
+
+await Task.WhenAll(
+    floodOneThroughFourTask,
+    floodFiveThroughEightTask);
+
+Console.ReadLine();
+
+static async ValueTask GetAsync(
+    HttpClient client, string url, CancellationToken cancellationToken)
+{
+    using var response =
+        await client.GetAsync(url, cancellationToken);
+
+    Console.WriteLine(
+        $"URL: {url}, HTTP status code: {response.StatusCode} ({(int)response.StatusCode})");
+}
